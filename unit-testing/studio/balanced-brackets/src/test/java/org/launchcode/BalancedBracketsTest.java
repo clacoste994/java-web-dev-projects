72dde10f29fb5d71dedb1cd1ffe8ed3a6185c1ee
clacoste994 <clacoste994@gmail.com>
2024-04-17 17:47:53 -0500

    Class1Studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,28 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void WrongBracketsCurly() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{}"));
+    }
+
+    @Test
+    public void WrongNumber() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void WrongBracketsSmooth() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("()"));
+    }
+
+
+
 }
+
